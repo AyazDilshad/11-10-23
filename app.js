92702669dfb71308dc2c283921665dfb5e3a6737
Ayaz Dilshad <ayaz18164@gmail.com>
2023-10-11 22:00:23 +0500

    bulb

---
+++ app.js
@@ -0,0 +1,11 @@
+function onHandler() {
+    onBtn.style.display = `none`
+    offBtn.style.display = `block`
+    bulb.src = `./assets/bulb-on.png`
+}
+
+function offHandler() {
+    offBtn.style.display = `none`
+    onBtn.style.display = `block`
+    bulb.src = `./assets/bulb-off.png`
+}
